a921942e68fefdcc18921a94e8c3ba5f8e98f97e
qaz5823091 <r19991106@gmail.com>
2020-12-31 01:51:41 +0800

    update HappyNewYear

--- getTime.js
+++ getTime.js
@@ -1,30 +1,28 @@
-
-// Set the date we're counting down to
 var countDownDate = new Date("Jan 1, 2021 00:00:00").getTime();
-
-// Update the count down every 1 second
 var x = setInterval(function() {
-
-  // Get today's date and time
   var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  var distanceSeconds = hours * 3600 + minutes * 60 + seconds;
   // Display the result in the element with id="demo"
-  var countDownTime = days + " 天 " + hours + " 小時 "
-  + minutes + " 分鐘 " + seconds + " 秒 ";
-  document.getElementById("time").innerHTML = countDownTime;
+
+  if (hours != 0) {
+    document.getElementById("time").innerHTML = "倒數 " + hours + " 小時 " + minutes + " 分鐘 " + seconds + " 秒 ";
+  }
+  else if (minutes != 0) {
+    document.getElementById("time").innerHTML = "倒數 " + minutes + " 分鐘 " + seconds + " 秒 ";
+  }
+  else {
+    document.getElementById("time").innerHTML = "倒數 " + seconds + " 秒 ";
+  }
+
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("time").innerHTML = "EXPIRED";
+    document.getElementById("time").innerHTML = "新年快樂!";
   }
 }, 1000);
